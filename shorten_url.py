53b2cf1541940857dd21156b890d455f175f283b
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-01-23 16:40:23 +0100

    adding requirments.txt to project

--- shorten_url.py
+++ shorten_url.py
@@ -7,6 +7,3 @@ def shorten_link(link):
     a = requests.post(url='https://goolnk.com/api/v1/shorten', data=data)
     a = a.text
     return (a[15:-2])
-
-a = shorten_link('https://upload.wikimedia.org/wikipedia/commons/9/99/InsSight_spacecraft_appendix_gallery_Image_55-full.jpg')
-print(a)

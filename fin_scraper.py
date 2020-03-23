027e025c795f98c964e90bb33e315977f160f6ff
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-03-23 17:17:46 +0100

    removing o function from job scheduled

--- fin_scraper.py
+++ fin_scraper.py
@@ -53,16 +53,10 @@ def insert_articles_to_database(articles_list):
             logging.info("Article has been added to database")
             article["url"] = shorten_link(article["url"])
             try:
-                #body_message = make_message(article)
-                '''
-                send_mail(
-                    username, password, body=body_message.encode("utf-8")
-                )
-                '''
                 send_mail(article)
                 logging.info("Mail has been send.")
             except Exception as error:
-                print(f"Error ocured while system tried to send mail with error message: {error}")
+                logging.warning(f"Error ocured while system tried to send mail with error message: {error}")
             database_connection.commit()
 
 
@@ -103,12 +97,11 @@ def main_job_get_current_articles():
 
 def job():
     print("I have started scraping")
-    get_archive_and_make_database()
     main_job_get_current_articles()
 
 if __name__ == "__main__":
     #get_archive_and_make_database()
-    schedule.every(1).minutes.do(job)
+    schedule.every(5).minutes.do(job)
 
     while True:
         schedule.run_pending()

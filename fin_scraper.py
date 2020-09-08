8ad332abb7b45d2c95f67b78490a90bde7f53b47
bartek <bartosz.kobylinski@gmail.com>
2020-09-08 14:51:13 +0200

    REMOVING SOME VARIABLES WHICH HAS NOT BEEN USED, SOME LINTING FIXES
    - some variables and import has been removed cause there was no use of it
    
    - linting fixes

--- fin_scraper.py
+++ fin_scraper.py
@@ -1,23 +1,16 @@
-import sqlite3
+import datetime
 import logging
-import time, datetime
-import smtplib
-import requests
-import requests_cache
+import sqlite3
+import time
+
 import schedule
 
 from current_ministry_of_finance import (
+    get_current_articles_from_legislacja, get_current_articles_from_projects,
     get_current_articles_on_ministry_of_finance,
-    get_current_articles_on_website_podatki_gov_pl,
-    get_current_articles_from_legislacja,
-    get_current_articles_from_projects
-)
-from send_mail import send_mail, make_message
-from credentials import credentials as credentials
+    get_current_articles_on_website_podatki_gov_pl)
+from send_mail import send_mail
 from shorten_url import shorten_link
-
-username = credentials["username"]
-password = credentials["password"]
 
 logging.basicConfig(filename='financial_newsletter_log', level=logging.INFO)
 
@@ -44,20 +37,20 @@ def insert_articles_to_database(articles_list):
     database_connection = sqlite3.connect("articles.db")
     cursor = database_connection.cursor()
     for article in articles_list:
-        cursor.execute("SELECT Title from Articles WHERE Title=?", (article["title"],))
+        cursor.execute("SELECT Title from Articles WHERE Title=?",
+                       (article["title"],))
         result = cursor.fetchone()
         if not result:
             cursor.execute(
-                "INSERT INTO Articles VALUES (?,?)", (article["title"], article["url"])
-            )
+                "INSERT INTO Articles VALUES (?,?)", (article["title"],
+                                                      article["url"]))
             logging.info("Article has been added to database")
             article["url"] = shorten_link(article["url"])
-            try:
-                send_mail(article)
-                t = datetime.datetime.now()
-                print(f"Mail has been send at {t}.")
-            except Exception as error:
-                logging.warning(f"Error ocured while system tried to send mail with error message: {error}")
+            time.sleep(30)
+            send_mail(article)
+            current_time = datetime.datetime.now()
+            current_time = current_time.strftime("%b %d %Y %H:%M")
+            logging.info(f"Mail has been send at {current_time}.")
             database_connection.commit()
 
 
@@ -73,7 +66,7 @@ def main_job_get_current_articles():
     insert_articles_to_database(articles)
     logging.info(
         """
-        Gathered articles are inserted in to database 
+        Gathered articles are inserted in to database
         and now I'm starting gathering current articles from podatki.gov.pl"""
         )
     articles = get_current_articles_on_website_podatki_gov_pl()
@@ -81,7 +74,7 @@ def main_job_get_current_articles():
     insert_articles_to_database(articles)
     logging.info(
         """
-        Gathered articles are inserted in to database 
+        Gathered articles are inserted in to database
         and now I'm starting gathering current articles from legislacja"""
         )
     articles = get_current_articles_from_legislacja()
@@ -89,22 +82,24 @@ def main_job_get_current_articles():
     insert_articles_to_database(articles)
     logging.info(
         """
-        Gathered articles are inserted in to database 
+        Gathered articles are inserted in to database
         and now I'm starting gathering current articles from projects"""
         )
     articles = get_current_articles_from_projects()
     logging.info("I have finished getting articles from projects")
     insert_articles_to_database(articles)
 
+
 def job():
     print("I have started scraping")
     main_job_get_current_articles()
 
+
 if __name__ == "__main__":
-    #get_archive_and_make_database()
-    schedule.every(120).minutes.do(job)
+    # get_archive_and_make_database()
+    main_job_get_current_articles()
+    schedule.every(1).minutes.do(job)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-   
